9a8fa5a539fe1aba34ac6cfb47be04eacc89476a
pranavsathyan1 <pranav.sathyan@ust.com>
2024-03-27 15:32:51 +0530

    program

---
+++ Library.cs
@@ -0,0 +1,21 @@
+namespace pranav.training{
+
+public class TrainingDashboard{
+
+string TrainerName;
+string startdate;
+string enddate;
+string[] participants;
+string[] topics;
+
+public static void publish() {
+     
+	 System.Console.WriteLine("public function");
+}
+public static void generateReport() {
+
+     System.Console.WriteLine("Generating Report");
+}
+
+}
+}
---
+++ Program1.cs
@@ -0,0 +1,13 @@
+using pranav.training;
+
+using System;
+class MyClass{
+
+static void Main(){
+
+Console.WriteLine(" Start");
+
+TrainingDashboard.publish();
+TrainingDashboard.generateReport();
+}
+}

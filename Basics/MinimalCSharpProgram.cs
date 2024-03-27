3b040964aa1bd8178ac61809b64c4008c6aab138
Pooja <pooja.salian@ust.com>
2024-03-27 09:45:28 +0530

    Adding first assignment

---
+++ Basics/MinimalCSharpProgram.cs
@@ -0,0 +1,10 @@
+using pooja.training;
+using System;
+class MyClass {
+	static void Main(){
+		Console.WriteLine("This is Main Dashboard");
+		MyUtilityClass.Publish();
+		MyUtilityClass.GenerateReport();
+		
+	}
+}
---
+++ Basics/mylibrary.cs
@@ -0,0 +1,21 @@
+namespace pooja.training{
+public class MyUtilityClass{
+	
+	 /*Step 1: Array Declaration
+       string trainerName;
+       string[] participants; 
+       string[] topics;*/
+       
+       	
+		public static void Publish(){
+		
+        System.Console.WriteLine("This is Publish Dashboard");
+	
+		}
+		public static void GenerateReport(){
+		
+        System.Console.WriteLine("This is GenerateReport Dashboard");
+	
+		}
+    }   
+}

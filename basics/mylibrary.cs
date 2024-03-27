e35f8551e6f7bea00eea415a11cfd314998edc5c
Sudha <ragasudha.thirumalaisamy@ust.com>
2024-03-27 09:21:59 +0530

    Adding first assignment

---
+++ basics/mylibrary.cs
@@ -0,0 +1,19 @@
+namespace sudha.training{
+public class TrainingDashboard{
+	// Properties
+	/*string TrainerName;
+	string startdate;
+	string enddate;
+	string[] participants;
+	string[] topics;*/
+	// Methods
+	public static void publish()
+	{
+		System.Console.WriteLine("Publish Training Report");
+	}
+	public static void generateReport()
+	{
+		System.Console.WriteLine("Generate Training Report");
+	}
+}
+}

cd9dca9fbde2df500ab118846ab379c792e4f81d
Nithin Bhaskaran <nithin.bhaskaran@ust.com>
2024-03-28 18:54:31 +0530

    Adding second file to branch

---
+++ Basics/mylibrary.cs
@@ -0,0 +1,21 @@
+using System;
+namespace nithin.training{
+public class TrainingDashboard{
+	string TrainerName;
+	string Startdate;
+	string Enddate;
+	string[] Participants;
+	string[] Topics;
+	
+	public static void publish(){
+	Console.WriteLine("Hello print");
+	
+	}
+	
+	public static void generatereport(){
+	Console.WriteLine("Hello print2");
+	
+	}
+	
+}
+}
---
+++ Basics/programme.cs
@@ -0,0 +1,11 @@
+using nithin.training;
+using System;
+class Myclass{
+	static void Main(){
+		Console.WriteLine ("starting...");
+		TrainingDashboard.publish();
+		TrainingDashboard.generatereport();
+		
+		
+	}
+}

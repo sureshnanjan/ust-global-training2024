be86fc4160bb243b2de98dea0e5ca6f704f85a92
Gopinath <gopinath.chinnasamy@gmail.com>
2024-03-28 22:50:29 +0530

    First VS Code push

---
+++ BasicProgram/MyFirstPrg/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyLib;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Automation Training_Assignment1");
+Report rp = new Report();
+rp.publish();
+rp.generateReport();
+
+
---
+++ BasicProgram/MyLib/Report.cs
@@ -0,0 +1,22 @@
+﻿namespace MyLib
+{
+    public class Report
+    {
+        //Properties
+        string TrainerName;
+        string startdate;
+        string enddate;
+        string[] participants;
+        string[] topics;
+
+        // Methods
+        public void publish()
+        {
+            System.Console.WriteLine("Publishing Report");
+        }
+        public void generateReport()
+        {
+            System.Console.WriteLine("Generating Report");
+        }
+    }
+}

cddf8ac800d78496df4d30feeafa6aa4f3b78942
Sudha <ragasudha.thirumalaisamy@ust.com>
2024-03-31 19:37:26 +0530

    Adding first commit

---
+++ basics/FirstProgram/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SudhaLibrary;
+    {
+        // Expected class, delegate, enum, interface, or struct
+        Console.WriteLine("Automation Training_Assignment1");
+     UtilityClass print = new UtilityClass();
+        print.publish();
+        print.generateReport();
+    }

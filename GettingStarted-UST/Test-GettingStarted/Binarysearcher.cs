ce1d518a9b09242969f15842fc860b2f4eead36b
Anu Alex <anualex287@gmail.com>
2024-04-01 20:26:45 +0530

    Adding program

---
+++ GettingStarted-UST/Test-GettingStarted/Binarysearcher.cs
@@ -0,0 +1,15 @@
+﻿namespace Test_GettingStarted
+{
+    internal class Binarysearcher
+    {
+        internal int dosearch;
+        private int[] myinputArray;
+        private object searchterm;
+
+        public Binarysearcher(int[] myinputArray, object searchterm)
+        {
+            this.myinputArray = myinputArray;
+            this.searchterm = searchterm;
+        }
+    }
+}
---
+++ GettingStarted-UST/Test-GettingStarted/Test Binary Search.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_GettingStarted
+{
+    [TestClass]
+    public class Test_Binary_Search
+    {
+        private object searchterm;
+
+        public void test_for_existing_values() 
+        {
+            int[] myinputArray = { 1, 2, 3 };
+            int searchTerm = 3;
+            int expected = 3;
+            Binarysearcher searcher = new Binarysearcher(myinputArray, searchterm);
+            int actual = searcher.dosearch;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void test_for_non_existing_values() 
+        {
+            int[] myinputArray = { 1, 2, 3 };
+            int searchTerm = 4;
+            int expected = ~4;
+            Binarysearcher searcher = new Binarysearcher(myinputArray, searchterm);
+            int actual = searcher.dosearch;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void non_existing_item()
+        {
+            int[] myinputArray = { 1, 2, 3 };
+            int searchTerm = 0;
+            int expected = ~1;
+            Binarysearcher searcher = new Binarysearcher(myinputArray, searchterm);
+            int actual = searcher.dosearch;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

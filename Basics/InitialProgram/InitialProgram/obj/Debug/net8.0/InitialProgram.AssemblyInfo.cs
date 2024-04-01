722a78b727256a6b4ba59aec0748d668d77eb3df
Pooja <pooja.salian@ust.com>
2024-04-01 17:55:19 +0530

    Latest Update

--- Basics/InitialProgram/InitialProgram/obj/Debug/net8.0/InitialProgram.AssemblyInfo.cs
+++ Basics/InitialProgram/InitialProgram/obj/Debug/net8.0/InitialProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InitialProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df31ef9ec4609cb6bd1930ffc7aca35db313f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d740dff556f123c01923ba23726b97fc8b88dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("InitialProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InitialProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

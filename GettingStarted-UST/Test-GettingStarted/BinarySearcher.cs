8967f2517f780b81aa35547e1586534993b9d048
cgopi1994 <gopinath.chinnasamy@gmail.com>
2024-04-01 19:30:02 +0530

    Binary Search scenarios incorporated

--- GettingStarted-UST/Test-GettingStarted/BinarySearcher.cs
+++ GettingStarted-UST/Test-GettingStarted/BinarySearcher.cs
@@ -14,7 +14,28 @@ namespace Test_GettingStarted
 
         internal int doSearch()
         {
-            throw new NotImplementedException();
+            int searchValue = Array.BinarySearch(myinputArray, searchTerm);
+            if (confirmsortedarray(myinputArray))
+            {
+                if (searchValue >= 0)
+                {
+                    return searchValue + 1;
+                }
+                else
+                {
+                    return searchValue - 1;
+                }
+            }
+            return -1;
+            
+        }
+
+        private bool confirmsortedarray(int[] myinputArray)
+        {
+            for(int i = 1;i< myinputArray.Length; i++)
+                if (myinputArray[i - 1] > myinputArray[i])
+                    return false;
+            return true;
         }
     }
 }

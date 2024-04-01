2d740dff556f123c01923ba23726b97fc8b88dc5
Pooja <pooja.salian@ust.com>
2024-04-01 17:41:12 +0530

    Latest Update

--- Basics/InitialProgram/Test-InitialProgram/obj/Debug/net8.0/Test_InitialProgram.AssemblyInfo.cs
+++ Basics/InitialProgram/Test-InitialProgram/obj/Debug/net8.0/Test_InitialProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_InitialProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b040964aa1bd8178ac61809b64c4008c6aab138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df31ef9ec4609cb6bd1930ffc7aca35db313f1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_InitialProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_InitialProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

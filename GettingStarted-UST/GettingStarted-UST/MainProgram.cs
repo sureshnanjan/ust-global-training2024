b2ee5e8f19e24e1138ef29fa34c69df71158487a
Nithin Bhaskaran <nithin.bhaskaran@ust.com>
2024-04-02 22:10:34 +0530

    Adding fourth file to branch

--- GettingStarted-UST/GettingStarted-UST/MainProgram.cs
+++ GettingStarted-UST/GettingStarted-UST/MainProgram.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLIbrary;
+
 
 namespace GettingStarted_UST
 {
     public class MainProgram
     {
         public static void Main(string[] args) {
-            // call all your demo methods here
-            Console.WriteLine("Inside the Main program");
-            UsingLiterals mypgm = new UsingLiterals();
-            mypgm.showUsage();
-        
+           Calculator calculator = new Calculator();
+            int result = calculator.add(10000,20000);
+            int result1 = calculator.subtract(2,1);
+
+            Console.WriteLine($"When I add 1,2 the resut is {result} when i subtract 2, 1 the result is {result1}");
+
+            UsingLiterals myobj = new UsingLiterals();
+            myobj.showUsage();
+
         }
     }
 }

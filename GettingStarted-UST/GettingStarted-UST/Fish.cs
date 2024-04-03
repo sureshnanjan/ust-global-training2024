d5ea55305eaebc9616c189d97e753a72c060101a
sureshnanjan <sureshnanjan@outlook.com>
2024-04-03 09:39:19 +0530

    Hands on Exercises

---
+++ GettingStarted-UST/GettingStarted-UST/Fish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_UST
+{
+    internal class Fish : Pet
+    {
+        public override void Feed() {
+        
+        }
+
+        public override void Ship()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GettingStarted-UST/GettingStarted-UST/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_UST
+{
+    internal abstract class Pet
+    {
+        public string name;
+        public string color;
+        public int id;
+        public abstract void Feed();
+        public abstract void Ship();
+    }
+
+    
+}
+

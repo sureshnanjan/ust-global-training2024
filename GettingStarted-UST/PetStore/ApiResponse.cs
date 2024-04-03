c29539f15aea0e5ab8b2564bf6f3b0db6a33ff0d
Administrator <anukalamparambil@gmail.com>
2024-04-03 09:34:18 +0530

    Adding new one to branch

---
+++ GettingStarted-UST/PetStore/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    /// <summary>
+    /// Contains Api Response
+    /// </summary>
+    internal class ApiResponse
+    {
+        int code;
+        string type;
+        string message;
+    }
+}

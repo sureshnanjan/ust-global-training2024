cd63b8b5ea594e1833bc9ae3dcc3cd3eff4f6222
Anit Mathew <anit.mathew@ust.com>
2024-04-03 17:51:12 +0530

    Adding a demo

---
+++ GettingStarted-UST/GettingStarted-UST/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_UST
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+        
+    }
+}

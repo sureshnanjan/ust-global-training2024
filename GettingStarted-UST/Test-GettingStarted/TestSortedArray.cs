73f101083cc55d176aa6621f53a944eb17ed2f32
Pooja <pooja.salian@ust.com>
2024-04-04 10:31:13 +0530

    Latest Update

---
+++ GettingStarted-UST/Test-GettingStarted/TestSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_GettingStarted
+{
+    /// <summary>
+    /// Sorted Array Test Cases
+    /// </summary>
+    [TestClass]
+    public class TestSortedArray
+    {
+        [TestMethod]
+        //By default sorting will be based on the first element 
+        public void Sorting_FirstElementBased()
+        {
+            //Array of Integers
+            int[] intArray = new int[] { 9, 2, 4, 3, 1, 5 };
+            Console.WriteLine("Sorted Array");
+            //Array.Sort takes Array as an input and sorts the array in ascending order 
+            Array.Sort(intArray);
+            foreach (int i in intArray)
+            {
+                Console.WriteLine(i + "");
+            }
+        }
+
+        //if first value is equal sort happens based on the second value
+
+        //Expected result should be Sorted Array
+    }
+}

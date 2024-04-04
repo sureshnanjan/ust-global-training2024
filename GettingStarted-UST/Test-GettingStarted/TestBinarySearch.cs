5db318df5479cd230459be0ec50c81934dbf472c
hilda <hilda.peter18@gmail.com>
2024-04-04 19:48:29 +0530

    Added assignment

--- GettingStarted-UST/Test-GettingStarted/TestBinarySearch.cs
+++ GettingStarted-UST/Test-GettingStarted/TestBinarySearch.cs
@@ -3,35 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 namespace Test_GettingStarted
 {
     [TestClass]
-    public  class TestBinarySearch
+    public class TestBinarySearch
     {
         [TestMethod]
-        public void test_for_existing_values() { 
-        
-            int[] myinputArray =  { 1, 2, 3 };
-            int searchTerm = 3;
-            int expected = 3;
-            BinarySearcher searcher = new BinarySearcher(myinputArray, searchTerm);
+        public void ExistingValueTest_Atend()
+        {
+
+            int[] myinputArray = { 1, 2, 3, 4, 5 };
+            int searchItem = 5;
+            int expected = 5;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
             int actual = searcher.doSearch();
-
             Assert.AreEqual(expected, actual);
-
+            Console.WriteLine("The result is" + actual);
         }
 
         [TestMethod]
-        public void test_for_existing_values_inbeginning() { }
+        public void ExistingValueTest_Atmiddle()
+        {
 
-        [TestMethod]
-        public void test_for_non_existing_items() {
             int[] myinputArray = { 1, 2, 3 };
-            int searchTerm = 4;
+            int searchItem = 2;
+            int expected = 2;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
+            int actual = searcher.doSearch();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExistingValueTest_Atbeginning()
+        {
+            int[] myinputArray = { 1, 2, 3 };
+            int searchItem = 1;
+            int expected = 1;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
+            int actual = searcher.doSearch();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void NonExistingValueTest_higherval()
+        {
+            int[] myinputArray = { 1, 2, 3 };
+            int searchItem = 4;
             int expected = ~4;
-            BinarySearcher searcher = new BinarySearcher(myinputArray, searchTerm);
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
             int actual = searcher.doSearch();
             Assert.AreEqual(expected, actual);
 
@@ -39,36 +63,82 @@ namespace Test_GettingStarted
 
 
         [TestMethod]
-        public void testing_non_existent_item() {
+        public void NonExistingValueTest_lowerval()
+        {
 
             // If value is not found and value is less than one or more elements in array, the negative number returned is the
             // bitwise complement of the index of the first element that is larger than value. 
             int[] myinputArray = { 1, 2, 3 };
-            int searchTerm = 0;
+            int searchItem = 0;
             int expected = ~1;
-            BinarySearcher searcher = new BinarySearcher(myinputArray, searchTerm);
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
             int actual = searcher.doSearch();
             Assert.AreEqual(expected, actual);
 
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public void nullinputarray_throws_ArgumentNullException() {
+        [TestMethod]
+        public void NonExistingValueTest_middleval()
+        {
+
+            // If value is not found and value is less than one or more elements in array, the negative number returned is the
+            // bitwise complement of the index of the first element that is larger than value. 
+            int[] myinputArray = { 2, 6, 9 };
+            int searchItem = 4;
+            int expected = ~2;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
+            int actual = searcher.doSearch();
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void NonSortedArray_NonExistentValueTest()
+        {
+            //Value less than non sorted array minimum value 
+            int[] myinputArray = { 1, 7, 3 };
+            int searchItem = 0;
+            //int expected = ~1;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
+            int actual = searcher.doSearch();
+            //Assert.AreEqual(expected, actual);
+            Assert.IsTrue(actual < 0);
+
+            //Value greated than non sorted array maximum value
+            searchItem = 9;
+            actual = searcher.doSearch();
+            Assert.IsTrue(actual < 0);
+        }
+
+        [TestMethod]
+        public void NonSortedArray_ExistentValueTest()
+        {
+
+            // If value is not found and value is less than one or more elements in array, the negative number returned is the
+            // bitwise complement of the index of the first element that is larger than value. 
+            int[] myinputArray = { 1, 7, 3 };
+            int searchItem = 7;
+            //int expected = ~1;
+            BinarySearcher searcher = new BinarySearcher(myinputArray, searchItem);
+            int actual = searcher.doSearch();
+            //Assert.AreEqual(expected, actual);
+            Assert.IsTrue(actual < 0);
+        }
+        /*Exceptions
+        RankException array is multidimensional.
+        ArgumentException value is of a type that is not compatible with the elements of array.
+        InvalidOperationException value does not implement comparison mechanism.
+        */
+        [TestMethod]
+
+        public void NullInputArray_Throws_ArgumentNullException()
+        {
+            //ArgumentNullException array is null.
             int[] input = null;
-            int searchTerm = 0;
-            BinarySearcher searcher = new BinarySearcher(input, searchTerm);
-
-            Assert.ThrowsException<ArgumentNullException>(() => { searcher.doSearch(); });
-            
-
-
+            int searchItem = 0;
+            BinarySearcher searcher = new BinarySearcher(input, searchItem);
+            Assert.ThrowsException<ArgumentNullException>(() => searcher.doSearch());
         }
 
-        public void someroutine() {
-        
-        
-        }
     }
 }

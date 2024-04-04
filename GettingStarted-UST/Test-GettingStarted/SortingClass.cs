d85f8d648ebdca127cd317d4a3e7b14ea7fd28ac
cgopi1994 <gopinath.chinnasamy@gmail.com>
2024-04-04 18:50:25 +0530

    Enhancement on Sorted Array Test

---
+++ GettingStarted-UST/Test-GettingStarted/SortingClass.cs
@@ -0,0 +1,32 @@
+﻿namespace Test_GettingStarted
+{
+    public class SortingClass : IComparable<SortingClass>
+    { 
+
+        int value; 
+        int value2;
+
+    public SortingClass(int v1, int v2)
+    {
+        this.value = v1;
+        this.value2 = v2;
+    }
+
+    public int Value { get { return this.value; } }
+
+    public override string? ToString()
+    {
+        return this.value.ToString() + "-" + this.value2.ToString(); // SimpleClass(1,2) prints as  "value-value2"
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return this.value.Equals(((SortingClass)obj).Value);
+    }
+
+    public int CompareTo(SortingClass? other)
+    {
+        return this.value.CompareTo(other.Value);
+    }
+}
+}
---
+++ GettingStarted-UST/Test-GettingStarted/TestSortedArrayDualValue.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_GettingStarted
+{
+    /// <summary>
+    /// Sorting of the Array
+    /// </summary>
+    
+    
+    [TestClass]
+    public class TestSortedArrayDualValue
+    {
+
+        [TestMethod]
+        public  void verifyFormat()
+        {
+            SortingClass simpleClass = new(2, 3);
+            string sExpected = "2-3";
+            Assert.AreEqual(sExpected, simpleClass.ToString());
+        }
+        
+        
+        [TestMethod]
+        public void uniqueSorting(){
+
+            SortingClass[] myInputArray = { new SortingClass(1, 9), new SortingClass(5, 4), new SortingClass(4, 2), new SortingClass(3, 1), new SortingClass(8, 6), };
+            //SortingClass[] expectedArray = { new SortingClass(1, 9), new SortingClass(5, 4), new SortingClass(4, 2), new SortingClass(3, 1), new SortingClass(8, 6), };
+            Console.WriteLine("Original Values in My Instances");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+            Array.Sort(myInputArray);
+            Console.WriteLine("\n AFTER SORTING  Values ");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+        }
+
+        [TestMethod]
+        public void firstValueDuplicateSorting()
+        {
+
+            SortingClass[] myInputArray = { new SortingClass(1, 1), new SortingClass(5, 4), new SortingClass(1, 2), new SortingClass(3, 1), new SortingClass(8, 6), };
+            Console.WriteLine("Original Values in My Instances");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+            Array.Sort(myInputArray);
+            Console.WriteLine("\n AFTER SORTING  Values ");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+        }
+        [TestMethod]
+        public void bothValuesDuplicateSorting()
+        {
+
+            SortingClass[] myInputArray = { new SortingClass(1, 1), new SortingClass(5, 4), new SortingClass(4, 2), new SortingClass(5, 4), new SortingClass(8, 6), };
+            Console.WriteLine("Original Values in My Instances");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+            Array.Sort(myInputArray);
+            Console.WriteLine("\n AFTER SORTING  Values ");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+        }
+        [TestMethod]
+        public void secondValueDuplicateSorting()
+        {
+
+            SortingClass[] myInputArray = { new SortingClass(1, 1), new SortingClass(5, 4), new SortingClass(9, 2), new SortingClass(3, 1), new SortingClass(8, 6), };
+            Console.WriteLine("Original Values in My Instances");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+            Array.Sort(myInputArray);
+            Console.WriteLine("\n AFTER SORTING  Values ");
+            foreach (var item in myInputArray)
+            {
+                Console.Write($"{item} , ");
+
+            }
+        }
+    }
+}

8cf36e2586a143d0492c963816d3b6542a9cc24b
Administrator <anukalamparambil@gmail.com>
2024-04-04 09:24:04 +0530

    Adding new one to branch

---
+++ GettingStarted-UST/GettingStarted-UST/SimpleClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_UST
+{
+    internal class SimpleClass : IComparable<SimpleClass>
+    {
+        int value, value2;
+
+        public SimpleClass(int val, int val2)
+        {
+            this.value = val;
+            this.value2 = val2;
+        }
+
+        public int Value { get { return this.value; } }
+        public int Value2 { get { return this.value2; } }
+
+        public override string? ToString()
+        {
+            return this.value2.ToString();
+            return this.value.ToString();
+        }
+
+        public int CompareTo(SimpleClass? other)
+        {
+            return this.value.CompareTo(other.Value);
+            return this.value2.CompareTo(other.Value2);
+        }
+    }
+}

2adffa8a546f6154b5b04cd5f55c604e5e55e7c8
Sudha <ragasudha.thirumalaisamy@ust.com>
2024-04-08 14:19:31 +0530

    Updated Class files

--- GettingStarted-UST/GettingStarted-UST/UsingLiterals.cs
+++ GettingStarted-UST/GettingStarted-UST/UsingLiterals.cs
@@ -64,60 +64,6 @@ namespace GettingStarted_UST
                 
             }
 
-            /// <summary>
-            /// Literals Assignment
-            /// </summary>
-
-
-            double numDouble = 89.9;
-            float myFloat = 89.7f;
-            double baseNum = 89;
-            double exponNum = 2;
-            double reSult = Math.Pow(baseNum, exponNum);
-
-            /// <summary>
-            /// Character type -  Literals
-            /// </summary>
-
-            Char myChar = 'C';
-            Char uniCode = '\u0061';
-            Char escSequence = '\0';
-
-            /// <summary>
-            /// String Literals
-            /// </summary>
-
-            string myString = "Ragasudha";
-            string newPath = @"C:\Users\Administrator\ust-global-training2024\";
-            string lastName = "Thirumalaisamy";
-            string name = $"{myString} {lastName} ";
-
-            /// <summary>
-            /// Null type Literals
-            /// </summary>
-            string s = null;
-
-            /// <summary>
-            /// Boolean type Literals
-            /// </summary>
-
-            bool gG = true;
-            bool rR = false;
-
-            /// <summary>
-            /// Printing C# Literals
-            /// </summary>
-
-            Console.WriteLine("Integer -Decimal literals \t\t     : " + decINt + " \nInteger- Hexa-decimal literals \t\t     : " + hexInt + " \nInteger- Binary literals \t\t     : " + binaryInt);
-            Console.WriteLine("Floating Literals- double \t\t     : " + numDouble + "\nFloating Literals- float \t\t     : " + myFloat + "\nFloating Literals- Exponentialvalue \t     : " + reSult);
-            Console.WriteLine("Character Literals - Single quote \t     : " + myChar + "\nCharacter Literals - Unicode Representation : " + uniCode + "\nCharacter Literals - Escape Sequence\t   : " + escSequence);
-            Console.WriteLine("String Literals -single \t\t   : " + myString + "\nString Literals -Path declaration\t   :" + newPath + "\nString Literals - Interpolation \t   : " + name);
-            Console.WriteLine("Null Literals\t\t\t\t   : " + s);
-            Console.WriteLine("Boolean Literals \t\t\t   : " + gG + "\nBoolean Literals\t\t\t   : " + rR);
-
-
-
-
         }
 
 

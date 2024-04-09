d42654421bf99ea88ab347493c967e320f87e6b2
HarishMohanPillai <harish.pillai21@gmail.com>
2024-04-09 20:35:27 +0530

    Adding Updates

--- GettingStarted-UST/Test-GettingStarted/TestBinarySearch.cs
+++ GettingStarted-UST/Test-GettingStarted/TestBinarySearch.cs
@@ -11,17 +11,7 @@ namespace Test_GettingStarted
     public  class TestBinarySearch
     {
         [TestMethod]
-        public void test_for_existing_values() { 
-        
-            int[] myinputArray =  { 1, 2, 3 };
-            int searchTerm = 3;
-            int expected = 3;
-            BinarySearcher searcher = new BinarySearcher(myinputArray, searchTerm);
-            int actual = searcher.doSearch();
-
-            Assert.AreEqual(expected, actual);
-
-        }
+        public void test_for_existing_values() {  
 
         [TestMethod]
         public void test_for_existing_values_inbeginning() { }

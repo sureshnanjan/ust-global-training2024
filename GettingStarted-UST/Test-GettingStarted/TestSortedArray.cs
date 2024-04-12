d81ac1817b4e3bdd30c1000cec76a33faa7e5305
unknown <neetu.st@gmail.com>
2024-04-12 19:03:26 +0530

    Committing changes related to binary search and sorted array

--- GettingStarted-UST/Test-GettingStarted/TestSortedArray.cs
+++ GettingStarted-UST/Test-GettingStarted/TestSortedArray.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace Test_GettingStarted
 {
     /// <summary>
-    /// Sorted Array Test Cases
+    /// Test Cases to check sorted array
     /// </summary>
     [TestClass]
     public class TestSortedArray
@@ -22,7 +22,7 @@ namespace Test_GettingStarted
             //Integers Array
             int[] intArray = new int[] { 9, 2, 4, 3, 1, 5 };
             Console.WriteLine("Sorted Array");
-            //Array.Sort takes Array as an input and sorts the array in ascending order 
+            //Array.Sort takes Array as an input and sorts the array in order 
             Array.Sort(intArray);
             foreach (int i in intArray)
             {
@@ -31,12 +31,12 @@ namespace Test_GettingStarted
         }
 
         [TestMethod]
-        //By default sorting will be based on the first element   
+        //By default sorting will be based on first element considering first element as the smallest element 
         public void Sorting_FromFirstElement()
         {
             //Integers Array
             int[] intArray = new int[] { 9, 2, 4, 3, 1, 5 };
-            //If array length is 6 elements theb by default sorting will be based on 1st element
+            //If array length is 6 elements then by default sorting will be based on 1st element
             //Sort the array till the last element. If first value is found to be equal then sort happens based on the second value and so on
 
             Array.Sort(intArray, 1, 5);

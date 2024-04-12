e1e40678e0a72f19ecb4383222c002c5eeb5914d
AvinashLalith <avinash.lalith@ust.com>
2024-04-12 11:32:43 +0530

    Sync Changes

---
+++ GettingStarted-UST/ConsoleApp1/PetDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PetStoreImplementation
+{
+
+    public class PetDemo
+    {
+
+        public PetDemo() {
+            //this.driver = driver;
+        }
+
+        public void InitiateBrowser() {
+
+           // driver.Navigate().GoToUrl("https://google.com/");
+            //string Title= driver.Title;
+            //Console.WriteLine(Title);
+            //driver.Quit();
+        
+        }
+    }
+}
--- GettingStarted-UST/GettingStarted-UST/MainProgram.cs
+++ GettingStarted-UST/GettingStarted-UST/MainProgram.cs
@@ -1,7 +1,11 @@
-﻿namespace GettingStarted_UST
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GettingStarted_UST
 {
     public class MainProgram
     {
+        static IWebDriver driver = new ChromeDriver();
         public static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
@@ -11,6 +15,12 @@
             Console.WriteLine($"When I add 1,2 the resut is {result} when i subtract 2, 1 the result is {result1}");
 
 
+        }
+
+        public static void petStoreImplementation(String URL) {
+         
+            driver.Navigate().GoToUrl(URL);
+            driver.Quit();
         }
     }
 }

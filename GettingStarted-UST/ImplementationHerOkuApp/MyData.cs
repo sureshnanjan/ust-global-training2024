5d5086649fb5b0e556882a19b4693b1664e757a3
Pooja <pooja.salian@ust.com>
2024-04-13 08:24:02 +0530

    Assignment HerOkuApp- Parameterised Test Added

---
+++ GettingStarted-UST/ImplementationHerOkuApp/MyData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ImplementationHerOkuApp
+{
+    public class MyData
+    {
+        public static IEnumerable HerOkuAppTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("A/B Testing", "A/B Testing");
+                yield return new TestCaseData("Add/Remove Elements", "Add/Remove Elements");
+                yield return new TestCaseData("Basic Auth", "Basic Auth");
+                yield return new TestCaseData("Broken Images", "Broken Images");
+            }     
+        }
+    }
+}

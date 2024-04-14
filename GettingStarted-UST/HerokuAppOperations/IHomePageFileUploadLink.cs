13bab5e8800be43d571b5120df0510bd89c5f750
Pooja <pooja.salian@ust.com>
2024-04-14 17:20:40 +0530

    Adding Interface Newly Created To HerokuApp operations

---
+++ GettingStarted-UST/HerokuAppOperations/IHomePageFileUploadLink.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    ///  This provides operations on File Upload Link 
+    /// </summary>
+    public interface IHomePageFileUploadLink
+    {
+        /// <summary>
+        /// Test method to validate - Choose File Button
+        /// </summary>
+        /// <returns></returns>
+        public void ChooseFileButton()
+        {
+
+        }
+
+        /// <summary>
+        /// Test method to validate - Upload Button
+        /// </summary>
+        /// <returns></returns>
+        public void UploadButton()
+        {
+
+        }
+       
+        /// <summary>
+        /// Test method to validate - Upload File
+        /// </summary>
+        /// <returns></returns>
+        public void UploadedFile()
+        {
+
+        }
+    }
+}

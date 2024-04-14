383b9a9311c6dece31d1e6d913057ad3fc60cbe9
Chandni Nair <chandni.nair@ust.com>
2024-04-14 21:53:36 +0530

    Herokku app_Page Object creation assignment- Page No: 28 and 17

---
+++ GettingStarted-UST/HerokuAppOperations/IFileDownload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    
+    public interface IFileDownload
+    {
+        /// <summary>
+        /// This method helps to verify whether correct URL loaded or not
+        /// </summary>       
+        string getURL();
+        string getTitle();
+        /// <summary>
+        /// Verify the title of the  page
+        /// </summary>
+        /// <returns></returns>
+
+        List<string> getAvailableFiles();
+        /// <summary>
+        /// Verify the list of different kind of files available for download
+        /// </summary>
+        /// <returns></returns>
+        void downloadFile(string fileName);
+
+        /// <summary>
+        /// Verify whether the listed element is clickable
+        /// </summary>
+        /// 
+        void downloadFilebyPosition(int pos);
+
+        void verifyDownloadstatus(string exp);
+
+    }
+}

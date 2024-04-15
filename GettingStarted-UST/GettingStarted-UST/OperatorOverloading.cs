ec8ee3870ef05add9121011df76f58c46c3e7392
Administrator <anukalamparambil@gmail.com>
2024-04-15 20:58:26 +0530

    feat : make sure operator overloading works

---
+++ GettingStarted-UST/GettingStarted-UST/OperatorOverloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_UST
+{
+    internal class OperatorOverloading
+    {
+        private int number;
+
+        public OperatorOverloading(int num)
+        {
+            this.number = num;
+        }
+
+        public OperatorOverloading()
+        {
+        }
+
+        // Overloading of Binary "+" operator
+        public static OperatorOverloading operator +(OperatorOverloading Calc1,
+                                             OperatorOverloading Calc2)
+        {
+            OperatorOverloading Calc3 = new OperatorOverloading(0);
+            Calc3.number = Calc2.number + Calc1.number;
+            return Calc3;
+        }
+
+        // function to display result
+        public void display()
+        {
+            Console.WriteLine("{0}", number);
+        }
+
+    }
+}

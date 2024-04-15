e199c0ba9a82a132bfeb33eb700013c60db8d312
Anu Alex <anualex287@gmail.com>
2024-04-15 21:44:51 +0530

    Heroku Object Creation

---
+++ GettingStarted-UST/HerokuAppOperations/IHome_KeyPresses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    internal interface IHome_KeyPresses
+    {
+        /// <summary>
+        /// Get the tite of the page
+        /// </summary>
+        /// <returns></returns>
+       public void GetKeyPresses();
+
+        ///<summary>
+        ///get the value entered inthe box
+        ///</summary>
+
+        public void GeTextBoxContent();
+
+    }
+}

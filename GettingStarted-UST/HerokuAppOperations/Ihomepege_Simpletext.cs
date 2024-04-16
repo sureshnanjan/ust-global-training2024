33db0e951cfd5deea94d2e1183d220d448d4fadb
pranavsathyan1 <pranav.sathyan@ust.com>
2024-04-16 09:35:43 +0530

    Simpletext

---
+++ GettingStarted-UST/HerokuAppOperations/Ihomepege_Simpletext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    internal interface Ihomepage_Simpletext
+    {
+        // Title Simple calss
+        public void Gettitle();
+        // Title in bold letters
+        public void title_in_bold();
+
+        // Content in two boxes
+        public void content_in_two_boxes();
+
+    }
+}

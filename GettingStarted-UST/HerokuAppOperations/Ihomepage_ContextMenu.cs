085a9dc9faee33b82ba3d7f47bcbb2e9f17b6893
pranavsathyan1 <pranav.sathyan@ust.com>
2024-04-16 09:17:43 +0530

    Object creation

---
+++ GettingStarted-UST/HerokuAppOperations/Ihomepage_ContextMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    internal interface Ihomepage_ContextMenu
+    {
+        // Title Con;text Menu
+        public void Gettitle();
+        // for getting description
+        public void Getdescription();
+        // when right cickon the text box
+        public void Click_on_text_box(string title);
+
+        // Getting an alert when I right click on the box
+
+        public void Getting_an_alert();
+
+
+
+
+    }
+}

4af569e68ea040b4c72875a4dfec89b1307e64b6
hilda <hilda.peter18@gmail.com>
2024-04-16 09:09:33 +0530

    Added Interface and comments for Challening And Slow Resource pages

--- GettingStarted-UST/HerokuAppOperations/IChallengingDomOperations.cs
+++ GettingStarted-UST/HerokuAppOperations/IChallengingDomOperations.cs
@@ -7,30 +7,68 @@ using System.Threading.Tasks;
 namespace HerokuAppOperations
 {
     /// <summary>
-    /// 
+    /// Represents the interface for Challenging Dom Page.
     /// </summary>
     public interface IChallengingDomOperations
     {
         /// <summary>
         /// This method fetched the current Url opened in the browser.
         /// </summary>
-        /// <returns></returns>
+        /// <returns> The fetched URL is as string</returns>
         string getURL();
+
+        /// <summary>
+        /// This method fetched the title opened in the browser.
+        /// </summary>
+        /// <returns> The fetched title as string</returns>
         string getTitle();
 
+        /// <summary>
+        /// This method fetches the main content in the page 
+        /// </summary>
+        /// <returns> The fetched main content is as string</returns>
         string getMainContent();
 
+        /// <summary>
+        /// This method performs operations for the first button.
+        /// </summary>
+        /// <returns></returns>
         void doOperation1();
 
+        /// <summary>
+        /// This method retireves the label text of the first button.
+        /// </summary>
+        /// <returns> The button label text as string</returns>
         string getOperation1Label();
 
+        /// <summary>
+        /// This method performs operations for the second button.
+        /// </summary>
+        /// <returns></returns>
         void doOperation2();
 
+        /// <summary>
+        /// This method retireves the label text of the second button.
+        /// </summary>
+        /// <returns> The button label text as string</returns>
         string getOperation2Label();
+
+        /// <summary>
+        /// This method performs operations for the third button.
+        /// </summary>
+        /// <returns></returns>
         void doOperation3();
 
+        /// <summary>
+        /// This method retireves the label text of the third button.
+        /// </summary>
+        /// <returns> The button label text as string</returns>
         string getOperation3Label();
 
+        /// <summary>
+        /// This method retireves the value of Answer for the actions performed 
+        /// </summary>
+        /// <returns> The answer value as string</returns>
         string getResult();
 
         string[] getTableHeadings();
---
+++ GettingStarted-UST/HerokuAppOperations/ISlowResourcePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    /// Represents the interface for SlowResourcePage.
+    /// </summary>
+    internal interface ISlowResourcePage
+    {
+        /// <summary>
+        /// Gets the title of the Slow Resource Page.
+        /// </summary>
+        /// <returns>The title of the Slow resource page as a string.</returns>
+        string GetTitle();
+
+        /// <summary>
+        /// Gets the context of the Slow Resource Page.
+        /// </summary>
+        /// <returns>The context of the Slow resource page as a string.</returns>
+        string GetContext();
+
+
+    }
+}

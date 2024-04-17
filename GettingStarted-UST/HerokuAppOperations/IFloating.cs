8ddcaf4baab253e7cd4fce248b12e379aecb149e
HarishMohanPillai <harish.pillai21@gmail.com>
2024-04-17 18:16:12 +0530

    new class added

---
+++ GettingStarted-UST/HerokuAppOperations/IFloating.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+    ///<summary>
+    ///Hold the Properties and Operations of Heroku App
+{
+    internal interface IFloating
+    {
+        string getUrl();
+        void setUrl(string url);
+        ///<summary>
+        ///</summary>
+    }
+}

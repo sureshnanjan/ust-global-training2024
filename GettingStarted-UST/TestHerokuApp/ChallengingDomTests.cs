ef6f9753adbd61dd805e3f5c16a4a059af0705f3
hilda <hilda.peter18@gmail.com>
2024-04-18 22:15:19 +0530

    Added test case for challengin Dom

---
+++ GettingStarted-UST/TestHerokuApp/ChallengingDomTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerokuAppOperations;
+using NUnit.Framework;
+
+namespace TestHerokuApp
+{
+    public class ChallengingDomTests
+    {
+        /// <summary>
+        /// Verify whether the page title is correct or not
+        /// </summary>
+        /// [Test]
+        public void PageTitleverification()
+        {
+            //Arrange
+            IChallengingDomOperations page = null;
+            string expectedTitle = "Challenging DOM";
+            //Action
+            string actualTitle = page.getTitle();
+            //Assert
+            Assert.That(actualTitle, Is.EqualTo(expectedTitle));
+        }
+    }
+}

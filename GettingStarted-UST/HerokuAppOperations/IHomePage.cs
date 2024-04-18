43c58ee2fd582fc6fdc4d9c7d0b8accdb05d3573
Sudha <ragasudha.thirumalaisamy@ust.com>
2024-04-18 12:15:16 +0530

    Updating Documentation

--- GettingStarted-UST/HerokuAppOperations/IHomePage.cs
+++ GettingStarted-UST/HerokuAppOperations/IHomePage.cs
@@ -6,7 +6,7 @@
     public interface IHomePage: IHerokuAppOperations
     {
         /// <summary>
-        /// 
+        /// Title 
         /// </summary>
         /// <returns></returns>
         string getTitle(); // zget Title // Print The document // saving title to some report

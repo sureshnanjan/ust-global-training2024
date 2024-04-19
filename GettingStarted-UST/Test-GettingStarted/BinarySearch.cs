9c89c67abaaf58639f51b91443fde86fb1637040
Administrator <anukalamparambil@gmail.com>
2024-04-19 14:44:25 +0530

    Pushing updates

--- GettingStarted-UST/Test-GettingStarted/BinarySearch.cs
+++ GettingStarted-UST/Test-GettingStarted/BinarySearch.cs
@@ -6,7 +6,9 @@ namespace Test_GettingStarted
     internal class BinarySearch
     {
         private int[] inPutArray;
+        private int[,] inputArray;
         private int searchItem;
+        private int key;
 
         public BinarySearch(int[] inPutArray, int searchItem)
         {
@@ -14,10 +16,25 @@ namespace Test_GettingStarted
             this.searchItem = searchItem;
         }
 
-        internal int doSearch()
+        public BinarySearch(int[,] inputArray, int key)
+        {
+            this.inputArray = inputArray;
+            this.key = key;
+        }
+
+        internal int doSearch(int[] inputArray, int key)
         {
             throw new NotImplementedException();
         }
 
+        internal int IsSorted(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string MultiException(int[,] inputArray)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

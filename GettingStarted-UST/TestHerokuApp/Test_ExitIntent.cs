d9f5cc632d4e31ff7025566e571b0058ece8a35c
Sumin Philip John <sumin.philipjohn@ust.com>
2024-04-23 09:34:33 +0530

    Updating the test method for heroku page

---
+++ GettingStarted-UST/TestHerokuApp/Test_ExitIntent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestHerokuApp
+{
+    internal class Test_ExitIntent
+    {
+        [Test]
+        public void ExitIntentTitleverification()  //Test method to execute the title verification of the exit intent page
+        {
+        }
+
+        [Test]
+        public void ExitIntentContentverification()  //Test method to execute the content verification of the exit intent page
+        {
+        }
+
+        public void ExitIntentModalWindowcheck()  //Test method to execute the modal window function of the exit intent page
+        {
+        }
+
+    }
+}
---
+++ GettingStarted-UST/TestHerokuApp/Test_JavascriptAlerts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestHerokuApp
+{
+    internal class Test_JavascriptAlerts
+    {
+
+        [Test]
+        public void JavascriptAlertTitleverification()  //Test method to execute the title verification of the javascript alert page
+        {
+        }
+
+        [Test]
+        public void JavascriptAlertClickOperation()  //Test method to execute the button click of the javascript alert page
+        {
+        }
+
+        [Test]
+        public void JavascriptAlertDisplaycontent()  //Test method to execute the results in the javascript alert page
+        {
+        }
+
+    }
+}

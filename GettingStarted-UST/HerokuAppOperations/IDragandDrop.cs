bb53945a242e8d9dad1e582053543d488840754a
jerin jose <jerin.jose3@ust.com>
2024-04-23 15:59:38 +0530

    add Idraganddrop and Inotification

---
+++ GettingStarted-UST/HerokuAppOperations/IDragandDrop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    internal interface IDragandDrop
+    {
+        /// <summary>
+        /// Retrieves the title of the current page.
+        /// </summary>
+        void GetTitle();
+
+        /// <summary>
+        /// Drags item A to item B.
+        /// </summary>
+        void DragA_B();
+
+        /// <summary>
+        /// Drags item B to item A.
+        /// </summary>
+        void DragB_A();
+
+        /// <summary>
+        /// Retrieves the title of box 1.
+        /// </summary>
+        /// <returns>The title of box 1 as a string.</returns>
+        string GetBox1Title();
+
+        /// <summary>
+        /// Retrieves the title of box 2.
+        /// </summary>
+        /// <returns>The title of box 2 as a string.</returns>
+        string GetBox2Title();
+
+
+    }
+}
---
+++ GettingStarted-UST/HerokuAppOperations/INotificationMessages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    internal interface INotificationMessages
+    {
+        /// <summary>
+        /// Retrieves the title of the current page 
+        /// </summary>
+        void GetTitle();
+
+        /// <summary>
+        /// Retrieves the content of the current page 
+        /// </summary>
+        void GetContent();
+
+        /// <summary>
+        /// Initiates a click action, typically used for notification popup
+        /// </summary>
+        void ClickHere();
+
+        /// <summary>
+        /// Retrieves the title of a notification.
+        /// </summary>
+        void GetNotificationTitle();
+
+        /// <summary>
+        /// Closes a notification.
+        /// </summary>
+        void CloseNotification();
+
+    }
+}

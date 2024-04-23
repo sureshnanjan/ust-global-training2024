3501c1651d340c45ac0ab1d684d6283af95b2ae3
Lekshmi1202 <Lekshmi.Mohan@ust.com>
2024-04-23 15:34:49 +0530

    Created IDropDown

---
+++ GettingStarted-UST/HerokuAppOperations/IDropDown.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    /// <summary>
+    ///  This provides access to all operations on heroku Home Page
+    /// </summary>
+    public interface IDropDown
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        string getTitle(); // zget Title // Print The document // saving title to some report
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns> 
+
+       void doActions(); // perform some click actions
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns> 

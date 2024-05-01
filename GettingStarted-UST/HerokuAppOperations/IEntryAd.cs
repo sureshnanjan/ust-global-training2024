b1a57275b106b4b77a49a3d28925401214b37f2e
249214GokulRajasekharan <gr42027@gmail.com>
2024-05-01 19:10:49 +0530

    web implementation for EntryAd page created

---
+++ GettingStarted-UST/HerokuAppOperations/IEntryAd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuAppOperations
+{
+    public interface IEntryAd
+    {
+        public string getTitle();
+
+        public string getPagecontentIentryAd();
+
+
+        public void clickHere();
+
+    }
+}
---
+++ GettingStarted-UST/HerokuWebdriverImplemention/EntryAdPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerokuAppOperations;
+using OpenQA.Selenium;
+
+namespace HerokuWebdriverImplemention
+{
+    internal class EntryAdPage : HerokuApp, IEntryAd
+    {
+        private By titleLocator;
+        private By contentLocator;
+        private By clickHereLocator;
+
+        public EntryAdPage()
+        {
+            titleLocator = By.XPath("//div[@class='example']//h3");
+            contentLocator = By.ClassName("example");
+            clickHereLocator =By.Id("restart-ad");
+        }
+
+        public void clickHere()
+        {
+            driver.FindElement(clickHereLocator).Click();
+        }
+
+        public string getPagecontentIentryAd()
+        {
+            return driver.FindElement(contentLocator).Text;
+        }
+
+        public string getTitle()
+        {
+            return driver.FindElement(titleLocator).Text;
+        }
+    }
+}

05a49c65e64bc2314ebba5f0815845d254c4ba91
manjush kumar <manjushkumar669@gmail.com>
2024-05-06 18:12:01 +0530

    GettingStarted

---
+++ GettingStarted-UST/HerokuWebdriverImplemention/FileUploadPage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerokuAppOperations;
+using OpenQA.Selenium;
+
+namespace HerokuWebdriverImplemention
+{
+    public class FileUploadPage : HerokuApp, IFileUploadPage
+    {
+        /// <summary>
+        /// Properties
+        /// </summary>
+
+        private By heading;
+        private By chooseFile;
+        private By upload;
+        private By message;
+
+        /// <summary>
+        /// File Upload Page construction web elements
+        /// </summary>
+        /// <param name="driver"></param>
+
+        public FileUploadPage(IWebDriver driver) : base(driver) {
+            this.heading = By.XPath("//*[@id=\"content\"]/div[1]/h3");
+            this.chooseFile = By.XPath("//*[@id=\"file-upload\"]");
+            this.upload = By.XPath("//*[@id=\"file-submit\"]");
+            this.message = By.XPath("//*[@id=\"content\"]/div/h3");
+        }
+
+        /// <summary>
+        /// Test Method to get Title
+        /// </summary>
+        /// <returns></returns>
+        public string getTitle()
+        {
+            return driver.FindElement(heading).Text;
+
+        }
+
+        /// <summary>
+        /// Test method to validate - Choose File Button
+        /// </summary>
+        /// <returns></returns>
+        public void ChooseFile(string filePath)
+        {
+            driver.FindElement(chooseFile).Click();
+            Thread.Sleep(3000);
+        }
+
+        /// <summary>
+        /// Test method to validate - Upload Button
+        /// </summary>
+        /// <returns></returns>
+        public void DoUpload()
+        {
+            driver.FindElement(upload).Click();
+            Thread.Sleep(3000);
+
+        }
+
+        /// <summary>
+        /// Test method to validate - Upload File Message
+        /// </summary>
+        /// <returns></returns>
+        public string getUploadedFileMessage()
+        {
+            return driver.FindElement(message).Text;
+        }
+    }
+}
---
+++ GettingStarted-UST/HerokuWebdriverImplemention/InputsPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HerokuAppOperations;
+using OpenQA.Selenium;
+
+namespace HerokuWebdriverImplemention
+{
+    internal class InputsPage : HerokuApp, IInputsPage
+    {
+        /// <summary>
+        /// Properties
+        /// </summary>
+        private By heading;
+        private By decrease;
+        private By increase;
+
+        /// <summary>
+        /// Inputs Page construction webelements
+        /// </summary>
+        /// <param name="driver"></param>
+        public InputsPage(IWebDriver driver) : base(driver) {
+            this.heading = By.XPath("//*[@id=\"content\"]/div/div/h3");
+            this.decrease = By.XPath("//*[@id=\"content\"]/div/div/div/input");
+            this.increase = By.XPath("//*[@id=\"content\"]/div/div/div/input");
+        }
+
+        /// <summary>
+        /// Test Method to get Title
+        /// </summary>
+        /// <returns></returns>
+        public string getTitle()
+        {
+            return driver.FindElement(heading).Text;
+        }
+
+        /// <summary>
+        /// Test method to validate - Input Increase Value
+        /// </summary>
+        /// <returns></returns>
+        public int IncreaseValue()
+        {
+            driver.FindElement(increase).Click();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Test method to validate - Input Decrease Value
+        /// </summary>
+        /// <returns></returns>
+        public int DecreaseValue()
+        {
+            driver.FindElement(decrease).Click();
+            throw new NotImplementedException();
+        }
+
+     
+    }
+}

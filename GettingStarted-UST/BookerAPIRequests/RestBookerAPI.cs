5ee5ba6e95e8faaac68adc6c4b090c1f37150000
cgopi1994 <gopinath.chinnasamy@gmail.com>
2024-05-06 20:42:45 +0530

    Feat : API Initialization

---
+++ GettingStarted-UST/BookerAPIRequests/RestBookerAPI.cs
@@ -0,0 +1,58 @@
+﻿namespace BookerAPIRequests;
+
+using System;
+using RestSharp;
+
+public class RestBookerAPI
+{
+    public static void createBooking()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void deleteBooking()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async void doAuth()
+    {
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/auth", Method.Post);
+        request.AddHeader("Content-Type", "application/json");
+        var body = @"{
+" + "\n" +
+        @"    ""username"" : ""admin"",
+" + "\n" +
+        @"    ""password"" : ""password123""
+" + "\n" +
+        @"}";
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+    }
+
+    public static void getBookingDetails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void getBookingIds()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void updateBooking()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void updatePartialBooking()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GettingStarted-UST/TestBookerAPI/TestRestFulAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookerAPIRequests;
+using NUnit.Framework;
+
+
+namespace TestBookerAPI
+{
+    [TestFixture]
+    internal class TestRestFulAPI
+    {
+        [Test]
+        public void verifyAuthenticationAPI()
+        {
+            Console.WriteLine("Started");
+            RestBookerAPI.doAuth();
+        }
+        [Test]
+        public void verifyGetBookingIdsAPI()
+        {
+            RestBookerAPI.getBookingIds();
+        }
+        [Test]
+        public void verifyBookingsDetailsAPI()
+        {
+            RestBookerAPI.getBookingDetails();
+        }
+        [Test]
+        public void verifyBookingCreationAPI()
+        {
+            RestBookerAPI.createBooking();
+        }
+        [Test]
+        public void verifyBookingUpdateAPI()
+        {
+            RestBookerAPI.updateBooking();
+        }
+        [Test] public void verifyPartialBookingUpdateAPI()
+        {
+            RestBookerAPI.updatePartialBooking();
+        }
+        [Test]
+        public void verifyBookingDeleteAPI()
+        {
+            RestBookerAPI.deleteBooking();
+        }
+    }
+}

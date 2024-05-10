61a3e3754e885d7061b99bd419a7dfaa82fb7a28
cgopi1994 <gopinath.chinnasamy@gmail.com>
2024-05-10 11:26:08 +0530

    Feat - API call out Implementation Basics

--- GettingStarted-UST/BookerAPIRequests/RestBookerAPI.cs
+++ GettingStarted-UST/BookerAPIRequests/RestBookerAPI.cs
@@ -5,17 +5,57 @@ using RestSharp;
 
 public class RestBookerAPI
 {
-    public static void createBooking()
+    public static async Task<RestResponse> createBooking()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking", Method.Post);
+        request.AddHeader("Content-Type", "application/json");
+        var body = @"{
+" + "\n" +
+        @"    ""firstname"" : ""Jim"",
+" + "\n" +
+        @"    ""lastname"" : ""Brown"",
+" + "\n" +
+        @"    ""totalprice"" : 111,
+" + "\n" +
+        @"    ""depositpaid"" : true,
+" + "\n" +
+        @"    ""bookingdates"" : {
+" + "\n" +
+        @"        ""checkin"" : ""2018-01-01"",
+" + "\n" +
+        @"        ""checkout"" : ""2019-01-01""
+" + "\n" +
+        @"    },
+" + "\n" +
+        @"    ""additionalneeds"" : ""Breakfast""
+" + "\n" +
+        @"}";
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static void deleteBooking()
+    public static async Task<RestResponse> deleteBooking()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking/1421", Method.Delete);
+        request.AddHeader("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static async void doAuth()
+    public static async Task<RestResponse> doAuth()
     {
         var options = new RestClientOptions("https://restful-booker.herokuapp.com")
         {
@@ -34,25 +74,95 @@ public class RestBookerAPI
         request.AddStringBody(body, DataFormat.Json);
         RestResponse response = await client.ExecuteAsync(request);
         Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static void getBookingDetails()
+    public static async Task<RestResponse> getBookingDetails()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking/358", Method.Get);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static void getBookingIds()
+    public static async Task<RestResponse> getBookingIds()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking", Method.Get);
+        var body = @"";
+        request.AddParameter("text/plain", body, ParameterType.RequestBody);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static void updateBooking()
+    public static async Task<RestResponse> updateBooking()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking/1620", Method.Put);
+        request.AddHeader("Cookie", "token=95195ec5a9853c2");
+        request.AddHeader("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+        request.AddHeader("Content-Type", "application/json");
+        var body = @"{
+" + "\n" +
+        @"    ""firstname"" : ""James"",
+" + "\n" +
+        @"    ""lastname"" : ""Brown"",
+" + "\n" +
+        @"    ""totalprice"" : 111,
+" + "\n" +
+        @"    ""depositpaid"" : true,
+" + "\n" +
+        @"    ""bookingdates"" : {
+" + "\n" +
+        @"        ""checkin"" : ""2018-01-01"",
+" + "\n" +
+        @"        ""checkout"" : ""2019-01-01""
+" + "\n" +
+        @"    },
+" + "\n" +
+        @"    ""additionalneeds"" : ""Breakfast""
+" + "\n" +
+        @"}";
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 
-    public static void updatePartialBooking()
+    public static async Task<RestResponse> updatePartialBooking()
     {
-        throw new NotImplementedException();
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com")
+        {
+            MaxTimeout = -1,
+        };
+        var client = new RestClient(options);
+        var request = new RestRequest("/booking/1620", Method.Patch);
+        request.AddHeader("Cookie", "token=95195ec5a9853c2");
+        request.AddHeader("Content-Type", "application/json");
+        var body = @"{
+" + "\n" +
+        @"    ""firstname"" : ""James"",
+" + "\n" +
+        @"    ""lastname"" : ""Brown""
+" + "\n" +
+        @"}";
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        return response;
     }
 }
--- GettingStarted-UST/TestBookerAPI/TestRestFulAPI.cs
+++ GettingStarted-UST/TestBookerAPI/TestRestFulAPI.cs
@@ -15,37 +15,48 @@ namespace TestBookerAPI
         [Test]
         public void verifyAuthenticationAPI()
         {
-            Console.WriteLine("Started");
-            RestBookerAPI.doAuth();
+            
+            Task<RestSharp.RestResponse> response = RestBookerAPI.doAuth();
+            Console.WriteLine(response.Result);
+            Assert.That(response.Result.IsSuccessStatusCode, Is.True);
         }
         [Test]
         public void verifyGetBookingIdsAPI()
         {
-            RestBookerAPI.getBookingIds();
+            Console.WriteLine("Started");
+            Task<RestSharp.RestResponse> response = RestBookerAPI.getBookingIds();
+            Console.WriteLine(response.Result.Content);
+            
         }
         [Test]
         public void verifyBookingsDetailsAPI()
         {
-            RestBookerAPI.getBookingDetails();
+            Task<RestSharp.RestResponse> response = RestBookerAPI.getBookingDetails();
+            Console.WriteLine(response.Result.ResponseStatus);
+            
         }
         [Test]
         public void verifyBookingCreationAPI()
         {
             RestBookerAPI.createBooking();
+            
         }
         [Test]
         public void verifyBookingUpdateAPI()
         {
             RestBookerAPI.updateBooking();
+          
         }
         [Test] public void verifyPartialBookingUpdateAPI()
         {
             RestBookerAPI.updatePartialBooking();
+         
         }
         [Test]
         public void verifyBookingDeleteAPI()
         {
             RestBookerAPI.deleteBooking();
+            
         }
     }
 }
